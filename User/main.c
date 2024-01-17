32aa526ceac47e1dee02d30a6b3fbc7c608931d2
Mahmood_Reda <mr4141705@gmail.com>
2024-01-17 12:54:22 +0300

    Editing main.c and topology.h files

--- User/main.c
+++ User/main.c
@@ -18,8 +18,6 @@ int main(void){
 
 	Module_Init();		//Initialize Module &  BitzOS
 
-
-
 	//Don't place your code here.
 	for(;;){}
 }
@@ -29,27 +27,8 @@ int main(void){
 /* User Task */
 void UserTask(void *argument){
 
-
 	// put your code here, to run repeatedly.
 	while(1){
-		Turn_PWM(1,100, MotorA);
-				Turn_PWM(1,100, MotorB);
-				Delay_ms(2000);
-				Turn_PWM(1,0, MotorA);
-				Turn_PWM(1,0, MotorB);
-				Delay_ms(2000);
-				Turn_PWM(1,50, MotorA);
-				Turn_PWM(1,50, MotorB);
-				Delay_ms(2000);
-				Turn_PWM(1,80, MotorA);
-				Turn_PWM(1,80, MotorB);
-				Delay_ms(2000);
-				Turn_PWM(1,20, MotorA);
-				Turn_PWM(1,20, MotorB);
-				Delay_ms(2000);
-				Turn_PWM(1,60, MotorA);
-				Turn_PWM(1,60, MotorB);
-				Delay_ms(2000);
 
 	}
 }
