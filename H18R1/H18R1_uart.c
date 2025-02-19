f766ad19c4495154e59a55b787697a96f639aa84
Mahmood_Reda <mr4141705@gmail.com>
2025-02-19 15:41:51 +0300

    Added HAL_UARTEx_WakeupCallback in IT file to disable Stop Mode.

--- H18R1/H18R1_uart.c
+++ H18R1/H18R1_uart.c
@@ -11,11 +11,11 @@
 #include "BOS.h"
 
 /*  */
-#ifndef __N
-uint16_t arrayPortsDir[MaxNumOfModules]; /* Array ports directions */
-#else
-uint16_t arrayPortsDir[__N ];
-#endif 
+//#ifndef __N
+//uint16_t arrayPortsDir[MaxNumOfModules]; /* Array ports directions */
+//#else
+//uint16_t arrayPortsDir[__N ];
+//#endif
 
 DMA_HandleTypeDef hdma_usart1_rx;
 DMA_HandleTypeDef hdma_usart2_rx;
@@ -191,6 +191,10 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart1_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart1_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart1_rx;
+
+
 	   HAL_DMA_Init(&hdma_usart1_rx);
 
 	/* USART1 interrupt Init */
@@ -236,6 +240,10 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart2_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart2_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart2_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart2_rx;
+
+
 	   HAL_DMA_Init(&hdma_usart2_rx);
 
 		__HAL_LINKDMA(huart,hdmarx,hdma_usart2_rx);
@@ -280,6 +288,10 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart3_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart3_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart3_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart3_rx;
+
+
 		HAL_DMA_Init(&hdma_usart3_rx);
 
 
@@ -356,6 +368,10 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart5_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart5_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart5_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart5_rx;
+
+
 	   HAL_DMA_Init(&hdma_usart5_rx);
 
 		__HAL_LINKDMA(huart,hdmarx,hdma_usart5_rx);
@@ -402,6 +418,10 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 		hdma_usart6_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
 		hdma_usart6_rx.Init.Mode = DMA_CIRCULAR;
 		hdma_usart6_rx.Init.Priority = DMA_PRIORITY_LOW;
+
+		msgRxDMA[(GetPort(huart)-1)] = &hdma_usart6_rx;
+
+
 	   HAL_DMA_Init(&hdma_usart6_rx);
 
 
