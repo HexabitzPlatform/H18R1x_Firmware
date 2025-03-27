2c6ebd0ecbbc68394107e23e33c848a5fe27caa3
Mahmood_Reda <mr4141705@gmail.com>
2025-03-27 17:38:16 +0300

    add ((GetModuleParameter)) empty function

--- H18R1/H18R1.c
+++ H18R1/H18R1.c
@@ -29,7 +29,7 @@ extern FLASH_ProcessTypeDef pFlash;
 extern uint8_t numOfRecordedSnippets;
 
 /* Module exported parameters ------------------------------------------------*/
-module_param_t modParam[NUM_MODULE_PARAMS] ={{.paramPtr = NULL, .paramFormat =FMT_FLOAT, .paramName =""}};
+module_param_t modParam[NUM_MODULE_PARAMS] ={0};
 
 /* Private variables ---------------------------------------------------------*/
 
@@ -403,6 +403,26 @@ void Module_Peripheral_Init(void){
 		  else if(GetUart(i)==&huart6)
 				   { index_dma[i-1]=&(DMA1_Channel6->CNDTR); }
 		}
+}
+/***************************************************************************/
+/* This functions is useful only for input (sensors) modules.
+ * @brief: Samples a module parameter value based on parameter index.
+ * @param paramIndex: Index of the parameter (1-based index).
+ * @param value: Pointer to store the sampled float value.
+ * @retval: Module_Status indicating success or failure.
+ */
+Module_Status GetModuleParameter(uint8_t paramIndex,float *value){
+	Module_Status status =BOS_OK;
+
+	switch(paramIndex){
+
+		/* Invalid parameter index */
+		default:
+			status =BOS_ERR_WrongParam;
+			break;
+	}
+
+	return status;
 }
 
 /*-----------------------------------------------------------*/
