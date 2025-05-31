3238b769365d12bba20995dfacaefcece5aaf3b9
Mahmood_Reda <mr4141705@gmail.com>
2025-05-31 16:28:09 +0300

    clear topology & main files .

--- User/main.c
+++ User/main.c
@@ -1,56 +1,38 @@
 /*
- BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
+ BitzOS (BOS) V0.4.0 - Copyright (C) 2017-2025 Hexabitz
  All rights reserved
 
  File Name     : main.c
  Description   : Main program body.
  */
-/* Includes ------------------------------------------------------------------*/
+
+/* Includes ****************************************************************/
 #include "BOS.h"
 
-/* Private variables ---------------------------------------------------------*/
+/* Private variables *******************************************************/
 
-/* Private function prototypes -----------------------------------------------*/
+/* Private Function Prototypes *********************************************/
 
-/* Main function ------------------------------------------------------------*/
-
+/* Main Function ***********************************************************/
 int main(void){
 
-	Module_Init();		//Initialize Module &  BitzOS
+	/* Initialize Module &  BitzOS */
+	Module_Init();
 
-	//Don't place your code here.
-	for(;;){}
+	/* Don't place your code here */
+	for(;;){
+	}
 }
 
-/*-----------------------------------------------------------*/
-/* Global variables */
-Motor motor;
-Module_Status motorTurnOff_Status;
-H_BridgeDirection direction;
-Module_Status motorTurnOn_Status;
-uint8_t dutyCycle;
-Module_Status motorSpeedControl_Status;
-
-/*-----------------------------------------------------------*/
-
+/***************************************************************************/
 /* User Task */
 void UserTask(void *argument){
 
-  // put your code here, to run repeatedly.
+	/* put your code here, to run repeatedly. */
+	while(1){
 
-
-    while (1) {
-//    	motorTurnOff_Status = MotorTurnOff(MOTOR_B);
-//    	HAL_Delay(2222);
-//    	motorTurnOn_Status = MotorTurnOn(2, MOTOR_B);
-    	HAL_Delay(2222);
-    	motorSpeedControl_Status = MotorSpeedControl(2, 20, MOTOR_B);
-    	    	HAL_Delay(2222);
-    	    	motorSpeedControl_Status = MotorSpeedControl(2, 70, MOTOR_B);
-
-    	    	    	HAL_Delay(2222);
-    	    	    	motorSpeedControl_Status = MotorSpeedControl(2, 99, MOTOR_B);
-
-  }
+	}
 }
-/*-----------------------------------------------------------*/
+
+/***************************************************************************/
+/***************** (C) COPYRIGHT HEXABITZ ***** END OF FILE ****************/
